b14b290fa9bc31b43d040920ab9bc00ee9d911e0
刘士 <liushilive@outlook.com>
2021-06-05 15:35:14 +0800

    1

--- md/exercise/code/013.py
+++ md/exercise/code/013.py
@@ -1,6 +1,5 @@
+from functools import reduce
+
 for n in range(100, 1000):
-    i = n // 100
-    j = int(n / 10 % 10)
-    k = int(n % 10)
-    if n == i ** 3 + j ** 3 + k ** 3:
+    if reduce(lambda x, y: x+y, map(lambda z: int(z) ** 3, str(n))) == n:
         print(n)
